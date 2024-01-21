562f5bddb842fe3df88abd30f1ca724d86906381
BEDPRAKASH <153425219+Bed-prakash@users.noreply.github.com>
2024-01-22 00:29:54 +0530

    Update Signin.js

--- Signin.js
+++ Signin.js
@@ -1,15 +1,21 @@
-
 function submitForm(){
     const email=document.getElementById("email").value
     const password=document.getElementById("password").value
     const cpassword=document.getElementById("cpassword").value
-    if (password==cpassword) {
+    const fname=document.getElementById("fname").value
+    const lname=document.getElementById("lname").value
+    const number=document.getElementById("number").value
+    const check=document.getElementById("check").value
+    const g1=document.getElementById("g1").value
+    const g2=document.getElementById("g2").value
+    const g3=document.getElementById("g3").value
+    console.log(number,lname,fname,check,g1,g2,g3);
+    if (password==cpassword && password.length!=0 && (fname.length)!=0 && (lname.length)!=0 && (number.length)==10) {
         sessionStorage.setItem("email",email);
         sessionStorage.setItem("password",password);
         document.getElementById("errorR").innerHTML="Registration Successfull";
         document.getElementById("errorR").style.color="green";
         var form1 =document.forms
-        console.log(form1);
         form1[0].elements[0].value="";
         form1[0].elements[1].value="";
         form1[0].elements[2].value="";
@@ -23,7 +29,20 @@ function submitForm(){
         form1[0].elements[10].value="";
         form1[0].elements[11].value="";
         form1[0].elements[12].value="";
-    } else {
+    } else if(password!=cpassword ){
         document.getElementById("errorR").innerHTML="Miss match password";
+        document.getElementById("errorR").style.color="red";
+    } else if(password.length==0){
+        document.getElementById("errorR").innerHTML="Invalid password";
+        document.getElementById("errorR").style.color="red";
+    }else if((fname.length)==0 ){
+        document.getElementById("errorR").innerHTML="First Name is Not Found";
+        document.getElementById("errorR").style.color="red";
+    } else if((lname.length)==0 ){
+        document.getElementById("errorR").innerHTML="Last Name is Not Found";
+        document.getElementById("errorR").style.color="red";
+    }else if((number.length)!=10){
+        document.getElementById("errorR").innerHTML="Mobile Number Invalid";
+        document.getElementById("errorR").style.color="red";
     }
 }
